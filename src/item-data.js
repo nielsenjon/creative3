513f293bb0ac56bbf7aae26d170ef637158129df
jonath8 <jonathan.brent.nelson@gmail.com>
2021-11-05 21:03:00 -0600

    Started images

--- src/item-data.js
+++ src/item-data.js
@@ -1,15 +1,22 @@
 let mock2 = [
   {
   id: 1,
-  game: "Legend of Zelda",
-  items: "Bow, Boomerang",
+  game: "LOZ, OoT",
+  items: "Boomerang",
+  photo: "boomerang.png",
   },
   {
   id: 2,
-  game: "Legend of Zelda",
-  items: "Bomb Bag",
+  game: "LOZ",
+  items: "Magical Boomerang",
+  photo: "magicalboomerang.png"
   },
-
+  {
+  id: 3,
+  game: "LOZ, OoT",
+  items: "Bomb",
+  photo: "bomb.png",
+  },
 ]
 
 export default mock2;
